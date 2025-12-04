39b0cec92f4b217caab92cfe988b0de87b479f07
Tomasz Szymanski <tszymans@student.42.fr>
2025-12-04 21:07:22 +0100

    [ex00] Header completed, preparing the function

---
+++ ex00/ClapTrap.cpp
@@ -0,0 +1,17 @@
+#include "ClapTrap.hpp"
+
+// Default constructor
+
+// Parametrized ctor
+
+// Copy ctor
+
+// Copy assignment operator
+
+// Destructor
+
+// Attack function
+
+// Take damage function
+
+// Repair function
--- ex00/ClapTrap.hpp
+++ ex00/ClapTrap.hpp
@@ -1,4 +1,29 @@
-#ifndef CLAPTRAP_H
-#define CLAPTRAP_H
+#ifndef CLAPTRAP_HPP
+#define CLAPTRAP_HPP
 
+#include <string>
+#include <iostream>
+
+class ClapTrap
+{
+public:
+	// Orthodox Canonical Form
+	ClapTrap();									// default ctor
+	ClapTrap(const std::string &name);			// parametrized ctor
+	ClapTrap(const ClapTrap &other);			// copy ctor
+	ClapTrap &operator=(const ClapTrap &other);	// copy assignment operator
+	~ClapTrap();								// destructor
+
+	// Member functions
+	void			attack(const std::string &target);
+	void			takeDamage(unsigned int amount);
+	void			beRepaired(unsigned int amount);
+
+private:
+	std::string		_name;
+	unsigned int	_hitPoints;
+	unsigned int	_energyPoint;
+	unsigned int	_attackDamage;
+
+};
 #endif
