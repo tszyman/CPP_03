bd4b1bd8b65e786370ea20910cfcdcdd0454f7b9
Tomasz Szymanski <tszymans@student.42.fr>
2025-12-06 15:54:25 +0100

    [ex02] Adding files, which are not necessary but required in the task

---
+++ ex02/ScavTrap.cpp
@@ -0,0 +1,59 @@
+#include "ScavTrap.hpp"
+
+// Default constructor
+ScavTrap::ScavTrap() : ClapTrap() {
+	_hitPoints = 100;
+	_energyPoints = 50;
+	_attackDamage = 20;
+	std::cout << "ScavTrap default constructor called" << std::endl;
+}
+
+// Parametrized ctor
+ScavTrap::ScavTrap(const std::string &name) : ClapTrap(name) {
+	_hitPoints = 100;
+	_energyPoints = 50;
+	_attackDamage = 20;
+	std::cout << "ScavTrap " << _name << " constructor called" << std::endl;
+}
+
+// Copy ctor
+ScavTrap::ScavTrap(const ScavTrap &other) : ClapTrap(other) {
+	std::cout << "ScavTrap " << _name << " copy constructor called" << std::endl;
+}
+
+// Copy assignment operator
+ScavTrap &ScavTrap::operator=(const ScavTrap &other) {
+	std::cout << "ScavTrap copy assignment operator called" << std::endl;
+	if (this != &other)
+	{
+		ClapTrap::operator=(other);
+	}
+	return(*this);
+}
+
+// Destructor
+ScavTrap::~ScavTrap() {
+	std::cout << "ScavTrap " << _name << " destructor called" << std::endl;
+}
+
+// Attack function
+void	ScavTrap::attack(const std::string &target)
+{
+	if (_hitPoints == 0)
+	{
+		std::cout << "ScavTrap " << _name << " is destroyed and cannot attack!" << std::endl;
+		return;
+	}
+	if (_energyPoints == 0)
+	{
+		std::cout << "ScavTrap " << _name << " can't attack because it has no energypoints left!" << std::endl;
+		return;
+	}
+	_energyPoints--;	// attack costs 1 point energy
+	std::cout << "ScavTrap " << _name << " attacks " << target << ", causing " << _attackDamage << " points of damage!" << std::endl;
+}
+
+void ScavTrap::guardGate()
+{
+	std::cout << "ScavTrap " << _name << " is now in Gate keeper mode!" << std::endl;
+}
